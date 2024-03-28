205268b72770db70a265ed0386f7943b3f5ad59d
vignesh <vigneshfornavy@gmail.com>
2024-03-28 12:58:30 +0530

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="text-center">
+      <h2 className="text-red-500 font-bold text-4xl">Heading By Vignesh</h2>
+    </div>
+  );
+}
+
+export default App;
